47ac2e481db7ada25eca8e5e3c7d217c4b398adb
Serdar <serdarsenturk@windowslive.con>
2019-10-03 20:55:02 +0300

    Added

---
+++ project-api/main.py
@@ -0,0 +1,51 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+products = {
+    231 : {
+        'id' : 231,
+        'name' : 'A',
+        'price' : 1000
+    }, 
+    232 : {
+        'id' : 232,
+        'name' : 'B',
+        'price' : 1001
+    },
+    233 : {
+        'id' : 233,
+        'name' : "C",
+        'price' : 1002
+    }
+}
+
+@app.route('/products', methods=['GET'])
+def returnAllProducts():
+    app.logger.warning("Return all products")
+    return jsonify(products)
+
+
+@app.route('/products', methods=['POST'])
+def createNewProduct():
+    product = request.get_json()
+
+    # products is a dictionary. A dictionary records contains a Key and a Value
+    # in that case your record only has Value
+
+    # what is the Key for this new record?
+
+    # Once bunu cevapla:: what is the output of the GET /products call after you add new product?
+    # okumuyordum burayı.whatsapp dan yaz
+
+    productId = product[id]
+    products[productId] = product
+    app.logger.warning("new product created")
+    return jsonify(product)
+
+@app.route('/products/231', methods = ['DELETE'])
+def deleteProduct():
+    # consider getting productId from the url.
+    # see : http://flask.palletsprojects.com/en/1.1.x/quickstart/#variable-rules
+    del products[231]
+    return jsonify(products)
